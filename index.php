5f6289dcdc8fdd509de05a27b5f441da82f71dc5
sharmelan99 <90138410+sharmelan99@users.noreply.github.com>
2023-10-30 11:58:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,342 @@
+<?php
+include_once('hms/include/config.php');
+if (isset($_POST['submit'])) {
+    $name = $_POST['fullname'];
+    $email = $_POST['emailid'];
+    $mobileno = $_POST['mobileno'];
+    $dscrption = $_POST['description'];
+    $query = mysqli_query($con, "insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
+    echo "<script>alert('Your information succesfully submitted');</script>";
+    echo "<script>window.location.href ='index.php'</script>";
+} ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> Hospital management System </title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="shortcut icon" href="assets/images/fav.jpg">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawsom-all.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+
+<body>
+
+    <!-- ################# Header Starts Here#######################--->
+
+    <header id="menu-jk">
+
+        <div id="nav-head" class="header-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3  col-sm-12" style="color:#000;font-weight:bold; font-size:42px; margin-top: 1% !important;">MedEase
+                        <a data-toggle="collapse" data-target="#menu" href="#menu"><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
+                    </div>
+                    <div id="menu" class="col-lg-8 col-md-9 d-none d-md-block nav-item">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#services">Services</a></li>
+                            <li><a href="#about_us">About Us</a></li>
+                            <li><a href="#logins">Logins</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </header>
+
+    <!-- ################# Slider Starts Here#######################--->
+
+    <div class="slider-detail">
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            </ol>
+
+
+
+
+
+            <div class="carousel-inner">
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="assets/images/slider/slider_2.jpg" alt="Second slide">
+                    <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">MedEase Plus</h5>
+                        <h6 class="animated bounceInDown">Hospital Management System</h6>
+
+
+
+                    </div>
+                </div>
+
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="assets/images/slider/slider_3.jpg" alt="Third slide">
+                    <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">MedEase Plus</h5>
+                        <h6 class="animated bounceInDown">Hospital Management System</h6>
+
+
+
+                    </div>
+
+                </div>
+
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+
+    </div>
+
+    <!--  ************************* Logins ************************** -->
+
+
+    <section id="logins" class="our-blog container-fluid">
+        <div class="container">
+            <div class="inner-title">
+
+                <h2>Logins</h2>
+            </div>
+            <div class="col-sm-12 blog-cont">
+                <div class="row no-margin">
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                            <img src="assets/images/patient.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>PatientHub Login</h6>
+                                <a href="hms/user-login.php" target="_blank">
+                                    <button class="btn btn-success btn-sm">Click Here</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                            <img src="assets/images/doctor.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>Doctors login</h6>
+                                <a href="hms/doctor" target="_blank">
+                                    <button class="btn btn-success btn-sm">Click Here</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                            <img src="assets/images/admin.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>AccountsAdmin Login</h6>
+
+                                <a href="hms/admin" target="_blank">
+                                    <button class="btn btn-success btn-sm">Click Here</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                            <img src="assets/images/admin.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>MedLabAdmin Login</h6>
+
+                                <a href="hms/medadmin" target="_blank">
+                                    <button class="btn btn-success btn-sm">Click Here</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+
+
+
+
+
+    <!-- ################# Our Departments Starts Here#######################--->
+
+
+    <section id="services" class="key-features department">
+        <div class="container">
+            <div class="inner-title">
+
+                <h2>Our Key Features</h2>
+                <p>Take a look at some of our key features</p>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-heartbeat"></i>
+                        <h5>Cardiology</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-ribbon"></i>
+                        <h5>Orthopaedic</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fab fa-monero"></i>
+                        <h5>Neurologist</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-capsules"></i>
+                        <h5>Pharma Pipeline</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-prescription-bottle-alt"></i>
+                        <h5>Pharma Team</h5>
+                    </div>
+                </div>
+
+
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="far fa-thumbs-up"></i>
+                        <h5>High Quality treatments</h5>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+        </div>
+
+    </section>
+
+
+
+
+    <!--  ************************* About Us Starts Here ************************** -->
+
+    <section id="about_us" class="about-us">
+        <div class="row no-margin">
+            <div class="col-sm-6 image-bg no-padding">
+
+            </div>
+            <div class="col-sm-6 abut-yoiu">
+                <h3>About Our Hospital</h3>
+                <p>The MedEase Plus Hospital Management System is a comprehensive software solution created to organise and optimise a healthcare institution's many operations. Patient data, appointment scheduling, medical records, billing, and inventory control are all efficiently managed by this system, which also handles other hospital functions. The Hospital Management System transforms healthcare administration in a similar way to how a supermarket management system streamlines retail operations. It eliminates the need for personnel to memorise a wide range of patient facts by digitally storing all relevant data, facilitating effective care delivery. The system improves patient care quality, optimises resource allocation, and provides accurate data management for informed decision-making with features including electronic health records, automated inventory updates, and intelligent reporting. This technology substantially enhances hospital operations by automating administrative activities and promoting seamless communication between medical personnel, mimicking the effectiveness and simplicity brought to retail administration by comparable software solutions.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- ################# Footer Starts Here#######################--->
+
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-6 col-sm-12">
+                    <h2>Useful Links</h2>
+                    <ul class="list-unstyled link-list">
+                        <li><a ui-sref="about" href="#">About us</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="portfolio" href="#">Services</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="products" href="#logins">Logins</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="gallery" href="#gallery">Gallery</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="contact" href="#">Contact us</a><i class="fa fa-angle-right"></i></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 col-sm-12 map-img">
+                    <h2>Contact Us</h2>
+                    <address class="md-margin-bottom-40">
+
+                        <?php
+                        $ret = mysqli_query($con, "select * from tblpage where PageType='contactus' ");
+                        while ($row = mysqli_fetch_array($ret)) {
+                        ?>
+                            Address: SLIIT Malabe Campus
+                            Phone: <?php echo $row['MobileNumber']; ?> <br>
+                            Email: <a href="mailto:<?php echo $row['Email']; ?>" class=""><?php echo $row['Email']; ?></a><br>
+                            Timing: <?php echo $row['OpenningTime']; ?>
+                    </address>
+
+                <?php } ?>
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+
+
+    </footer>
+    <div class="copy">
+        <div class="container">
+            MedEase Hospital Management System
+            ITP Module
+
+        </div>
+
+    </div>
+
+</body>
+
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/jquery.easing.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/scrolling-nav.js"></script>
+<script src="assets/plugins/scroll-fixed/jquery-scrolltofixed-min.js"></script>
+
+<script src="assets/js/script.js"></script>
+
+
+
+</html>
